06663319a09b34a2029e294e568f1c9d37ce5056
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-16 16:13:11 +0000

    feat add : pool config db

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,13 @@ const dbInstance = new Sequelize({
     password: "root",
     database: "db_eventus",
     dialect: "mysql",
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    } 
 });
 
 export { dbInstance };
