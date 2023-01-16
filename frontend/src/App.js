2b742afa41318ee7d792c23c07acc21b9bc39c7d
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-16 16:16:57 +0000

    feat add : routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import CreateUser from "./POST/CreateUser.js";
+import GetUser from "./GET/GetUser.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/createuser" element={<CreateUser />} />
+        <Route path="/getuser" element={<GetUser />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
