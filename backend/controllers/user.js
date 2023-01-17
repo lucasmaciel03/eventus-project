d1d88064997ae25c7832c14f09f6e3628cdb47e0
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-17 18:01:01 +0000

    feat add :  controller add new user ( NEED SOME ADJUSTMENTS )

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,13 +1,66 @@
 import { UserModel } from '../models/user.js'
+import { LocationModel } from '../models/location.js'
+import pkg from 'validator'
+import multer from 'multer'
+import path from 'path' 
+const { isEmail } = pkg // https://www.npmjs.com/package/validator
+
+// POST - Create a new user
+export const createUser = async (req, res) => {
+    let info = {
+        username: req.body.username,
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        password: req.body.password,
+        locationId: req.body.locationId,
+        birthDate: req.body.birthDate,
+        profilePicture: req.file.filename
+    }
+
+    // verify if username already exists, verify if email is valid, verify if locationID exists in the database 
+    const user = await UserModel.findOne({
+        where: {
+            username: info.username || '' // if username is null, set it to empty string to avoid error in the query
+        }
+    })
+
+    const location = await LocationModel.findOne({
+        where: {
+            id: info.locationId || '' // if locationId is null, set it to empty string to avoid error in the query
+        }
+    })
+
+    if (user) {
+        res.status(400).send({
+            message: "Username already exists"
+        })
+    }
+    else if (!isEmail(info.email)) {
+        res.status(400).send({
+            message: "Email is not valid"
+        })
+    }
+    else if (!location) {
+        res.status(400).send({
+            message: "Location does not exist"
+        })
+    }
+    else {
+        const newUser = await UserModel.create(info)
+        res.status(201).send(newUser)
+    }
+}
+
+// GET - Get all users
+export const getAllUsers = async (req, res) => {
+    const users = await UserModel.findAll()
+    res.status(200).send(users)
+}
+
 //  Image Upload
 
-// 1. Import multer
-import multer from 'multer'
-import path from 'path'
-
-
-
-// 2. Set up multer, save image into frontend/public/images/uploads
+// 2. Set up storage engine for multer
 export const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/')
@@ -18,7 +71,7 @@ export const storage = multer.diskStorage({
 })
 
 
-// 3. Set up multer, save image into frontend/public/images/uploads
+// 3. Set up multer to upload the image
 export const upload = multer({
     storage: storage,
     limits: { fileSize: '1000000' },
@@ -32,27 +85,7 @@ export const upload = multer({
         }
         cb('Give proper files formate to upload')
     }
-}).single('image')
-
-// 4. Create a new user
-export const createUser = async (req, res) => {
-
-    let info = {
-        image: req.file.path,
-        imageType: path.parse(req.file.originalname).ext
-    }
-
-    const product = await UserModel.create(info)
-    res.status(200).send(product)
-    console.log(product)
-
-}
-
-// 5. Get all users
-export const getAllUsers = async (req, res) => {
-    const users = await UserModel.findAll()
-    res.status(200).send(users)
-}
+}).single('profilePicture')
 
 
 
