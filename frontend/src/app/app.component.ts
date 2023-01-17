3ca94b7aa17b55f7d92d39a7c5c4b0151e520611
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-17 22:36:52 +0000

    feat edit : change route

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { NavController } from '@ionic/angular';
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    public navCtrl: NavController,
+    private platform: Platform,
+  ) {
+    this.navCtrl.navigateRoot('/register');
+  }
 }
