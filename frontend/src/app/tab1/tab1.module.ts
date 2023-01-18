095e22a85e41c7fcf43a21ff8229fca1853ecd7a
AndreMaia95 <andrefsm95@gmail.com>
2023-01-18 15:36:35 +0000

    fix: homepage to Tab1 and delete files

--- frontend/src/app/tab1/tab1.module.ts
+++ frontend/src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { PostsComponent } from './../components/posts/posts.component';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -13,8 +14,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, PostsComponent],
 })
 export class Tab1PageModule {}
