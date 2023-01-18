981053a5d4ef1ae5bf3a64387eab4cafe9207894
AndreMaia95 <andrefsm95@gmail.com>
2023-01-18 22:43:28 +0000

    feat (add): cards

--- frontend/src/app/components/cards/cards.component.ts
+++ frontend/src/app/components/cards/cards.component.ts
@@ -1,23 +1,23 @@
+import { ToastController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss'],
 })
 export class CardsComponent implements OnInit {
-  favorite: boolean = false;
-  constructor(public AlertController: AlertController) {}
+  constructor(private toastCtrl: ToastController) {}
 
   ngOnInit() {}
-  onClick() {
-    this.favorite = !this.favorite;
-    if (this.favorite) {
-      this.AlertController.create({
-        header: 'Adicionado aos favoritos',
-        message: 'Este item foi adicionado aos seus favoritos.',
-        buttons: ['OK'],
-      }).then((alert) => alert.present());
-    }
+
+  async presentToast() {
+    let toast = await this.toastCtrl.create({
+      message: 'Adicionado aos favoritos',
+      duration: 1500,
+      position: 'top',
+    });
+
+    toast.present();
   }
 }
