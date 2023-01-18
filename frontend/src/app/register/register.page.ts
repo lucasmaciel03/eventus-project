4ef7b16a716d9977cfb2b9b21fcde19d13ddf631
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-18 22:11:08 +0000

    feat add : connect form to controller

--- frontend/src/app/register/register.page.ts
+++ frontend/src/app/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavParams } from '@ionic/angular';
+import { CrudService } from '../services/api/crud.service';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +8,20 @@ import { NavParams } from '@ionic/angular';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+// Variável para criar conta
+  usernameInput : any;
+  nameInput : any;
+  surnameInput : any;
+  emailInput : any;
+  passwordInput : any;
+  password_confirmationInput : any;
 
 // Variável que controla o tipo do input tipo boolean
   type:boolean=true;
-
+  
 
   constructor(
- 
+    private crudService: CrudService,
   ) { }
 
   ngOnInit() {
@@ -21,6 +29,24 @@ export class RegisterPage implements OnInit {
 
   exibirouocultar(){
     this.type=!this.type
+  }
+
+  createAccount(){
+    const createAccount = {
+      username : this.usernameInput,
+      name : this.nameInput,
+      surname : this.surnameInput,
+      email : this.emailInput,
+      password : this.passwordInput,
+    }
+    this.crudService.createAccount('createAccount', createAccount).subscribe((res) => {
+      console.log(res);
+      if (res == 200) {
+        console.log("Conta criada com sucesso!");
+      } else {
+        console.log("Erro ao criar conta!");
+      }
+    });
   }
 
 }
