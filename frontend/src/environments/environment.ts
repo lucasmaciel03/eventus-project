59e6b951d417ecfc0a08ee98543975080483fd00
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-18 17:48:45 +0000

    feat add : crud service

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_url: 'http://localhost:4242',
+  session : {
+    TOKEN : 'token',
+  },
 };
 
 /*
