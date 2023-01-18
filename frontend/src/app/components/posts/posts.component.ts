1c9d97e0fed3520e45de4cd9f3913e09a617d6b0
AndreMaia95 <andrefsm95@gmail.com>
2023-01-18 16:24:25 +0000

    feat (add):favorite icon change color when clicked

--- frontend/src/app/components/posts/posts.component.ts
+++ frontend/src/app/components/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-posts',
@@ -6,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.scss'],
 })
 export class PostsComponent implements OnInit {
+  favorite: boolean = false;
 
-  constructor() { }
+  constructor(public AlertController: AlertController) {}
 
   ngOnInit() {}
-
+  onClick() {
+    this.favorite = !this.favorite;
+    if (this.favorite) {
+      this.AlertController.create({
+        header: 'Adicionado aos favoritos',
+        message: 'Este item foi adicionado aos seus favoritos.',
+        buttons: ['OK'],
+      }).then((alert) => alert.present());
+    }
+  }
 }
