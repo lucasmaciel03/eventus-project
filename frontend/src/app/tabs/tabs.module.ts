b3684c5cc127238697bbcb76e360d4282691a321
AndreMaia95 <andrefsm95@gmail.com>
2023-02-01 15:55:51 +0000

    feat (add): app translated

--- frontend/src/app/tabs/tabs.module.ts
+++ frontend/src/app/tabs/tabs.module.ts
@@ -2,6 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
@@ -12,8 +13,9 @@ import { TabsPage } from './tabs.page';
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    TranslateModule,
   ],
-  declarations: [TabsPage]
+  declarations: [TabsPage],
 })
 export class TabsPageModule {}
--- frontend/src/app/tabs/tabs.page.ts
+++ frontend/src/app/tabs/tabs.page.ts
@@ -1,12 +1,33 @@
+import { LocalizationService } from './../services/localization/localization.service';
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Preferences } from '@capacitor/preferences';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  constructor(
+    private toastController: ToastController,
+    private translateService: TranslateService,
+    private LocalizationService: LocalizationService
+  ) {}
 
-  constructor() {}
+  async changeLanguage(language: string) {
+    await Preferences.set({ key: 'user-lang', value: language });
+    await this.LocalizationService.setLanguage(language);
+    await this.showToast();
+    console.log(language);
+  }
 
+  async showToast() {
+    const toast = await this.toastController.create({
+      message: this.translateService.instant('language as been changed'),
+      duration: 4000,
+    });
+    await toast.present();
+  }
 }
