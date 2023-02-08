d20aee52c8aae7fb242c289abbc5c06e1218e7c1
Lucas Maciel <03lucasmaciel@gmail.com>
2023-02-08 02:49:16 +0000

    feat add : get all events created by user

--- frontend/src/app/components/cardhistory/cardhistory.component.ts
+++ frontend/src/app/components/cardhistory/cardhistory.component.ts
@@ -1,4 +1,12 @@
+import { Preferences } from '@capacitor/preferences';
+import { LocalizationService } from './../../services/localization/localization.service';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { ToastController, NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/services/api/crud.service';
+import jwt_decode from 'jwt-decode';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cardhistory',
@@ -7,8 +15,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardhistoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private toastCtrl: ToastController,
+    private toastController: ToastController,
+    private navController: NavController,
+    private router: Router,
+    private translateService: TranslateService,
+    private LocalizationService: LocalizationService,
+    private crudService: CrudService,
+    private route: ActivatedRoute
+  ) {
+  }
+  user: any;
+  events: any[] = [];
+  monthNames = ['JAN', 'FEV', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 
-  ngOnInit() {}
+
+  async ngOnInit() {
+    await this.getToken();
+    this.getEventsByUserId();
+  }
+
+  getToken = async () => {
+    const token = await Preferences.get({ key: 'token' });
+
+    if (token.value !== null) {
+      const user = jwt_decode(token.value);
+      this.user = user;
+    }
+  };
+
+  checkToken = async () => {
+    const hasToken = await Preferences.get({ key: 'token' });
+    if (hasToken.value === null) {
+      this.router.navigateByUrl('/login', { replaceUrl: true });
+    } else {
+      this.router.navigateByUrl('tabs/tab4', { replaceUrl: true });
+    }
+  };
+
+  getEventsByUserId = async () => {
+    console.log('*****************' + 'ENTROU' + this.user._id)
+    if (this.user) {
+    this.crudService.getLikedEvents('getEventsByUserId', this.user._id).subscribe(
+    (data) => {
+    console.log('****************' + data);
+    this.events = data;
+    this.events.forEach(event => {
+      event.image = `http://localhost:4243/uploads/events/${event.image}`;
+    });
+    });
+    }
+  }
+
+  getFormattedDate(date: string) {
+    let dateObj = new Date(date);
+    let day = dateObj.getDate();
+    let monthIndex = dateObj.getMonth();
+    return `${day} ${this.monthNames[monthIndex]}`;
+  }
 
 }
